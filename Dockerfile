0c23947d4cec4d7cf5b7602154c5bb5750dcd891
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-05-23 11:10:26 +0000

    Finalized database config with docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9-alpine3.13
+FROM python:3.9-alpine3.13 
 
 # LABEL maintainer="williamdreams89@outlook.com"
 
@@ -9,11 +9,15 @@ COPY ./app /app
 WORKDIR /app
 EXPOSE 8000
 
-
+ARG DEV=false
 RUN python -m venv /py && \
     /py/bin/pip install --upgrade pip && \
+    apk add --update --no-cache postgresql-client && \
+    apk add --update --no-cache --virtual .tmp-build-deps \
+        build-base postgresql-dev musl-dev && \
     /py/bin/pip install --no-cache-dir -r /tmp/requirements.txt && \
     rm -rf /tmp && \ 
+    apk del .tmp-build-deps && \
     adduser \
         --disabled-password \
         --no-create-home \
--- app/usersapi/serializers.py
+++ app/usersapi/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
@@ -11,4 +11,15 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = get_user_model().objects.create_user(**validated_data)
         return user 
+
+
+class UserLoginSerialzer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=16, min_length=8)
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user 
+        return serializers.ValidationError("Invalid username or password.")
 
