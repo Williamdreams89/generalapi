f67d0ac0e18aae9ac844a5c403d91310c28c0a44
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2023-07-01 21:13:10 +0000

    More changes

--- app/usersapi/admin.py
+++ app/usersapi/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=['email', "is_superuser", "date_joined"]
+
+admin.site.register(get_user_model(), UserAdmin)
--- app/usersapi/views.py
+++ app/usersapi/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
 from rest_framework import status
 from django.contrib.auth import get_user_model
-from .serializers import UserRegisterSerializer
+from .serializers import *
 
 class UserRegisterAPIView(GenericAPIView):
     serializer_class = UserRegisterSerializer
@@ -11,7 +11,15 @@ class UserRegisterAPIView(GenericAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.validated_data
         serializer.save()
-
-
-
         return Response({"message":"New User Created Successfully"}, status=status.HTTP_201_CREATED)
+
+
+class UserLoginAPIView(GenericAPIView):
+    serializer_class = UserLoginSerialzer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data
+
+        return Response("Login successful", status=status.HTTP_200_OK)
